c180b22602dfac0fdf7e1ce6ae749ed51b914353
Leela Peddireddy <peddireddyleela2006@gmail.com>
2026-07-04 16:54:19 +0530

    Configure Firebase Admin using environment variables

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,11 +1,17 @@
 const { initializeApp, cert, getApps } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-};
+let serviceAccount;
+
+if (process.env.FIREBASE_PROJECT_ID) {
+  serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  };
+} else {
+  serviceAccount = require("./firebaseAdmin.json");
+}
 
 if (!getApps().length) {
   initializeApp({
