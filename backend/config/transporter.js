f2549486641617c744a7267853e5946ff8a04d01
Leela Peddireddy <peddireddyleela2006@gmail.com>
2026-07-20 17:08:27 +0530

    otp smtp to brevo

--- backend/config/transporter.js
+++ backend/config/transporter.js
@@ -1,13 +1,12 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.BREVO_LOGIN,
+    pass: process.env.BREVO_SMTP_KEY,
   },
 });
 
